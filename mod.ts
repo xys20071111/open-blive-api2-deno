511f90eccc6cef686207ab0ac301303aa18432e3
xys20071111 <xys20071111@hotmail.com>
2022-08-24 14:57:39 +0800

    导出需要的类型

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export { BLiveApi } from './src/blive_requester.ts'
 export { InteractiveInstence } from './src/interactive_instance.ts'
-export type { DanmakuReceiver } from './src/danmaku_receiver.ts'
+export { DanmakuReceiver } from './src/danmaku_receiver.ts'
+export type { IAnchorInfo } from './src/IAnchorInfo.ts'
